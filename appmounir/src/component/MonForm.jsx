0db94faad6767ed531d06778b783dc253c9ee691
Nicolas <mkleinnicolas@gmail.com>
2022-03-07 17:25:09 +0100

    Form2

--- appmounir/src/component/MonForm.jsx
+++ appmounir/src/component/MonForm.jsx
@@ -1,14 +1,35 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import { useState } from "react";
 
 const MonForm = (props) => {
+
+    const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs);
+  }
+  
     return (
         <form>
             <label>
-                Titre :<input type="text" name="Titre"/>
-                Durée :<input type="text" name="name"/>
-                Difficulté :<input type="text" name="name"/>
-                Pièce :<input type="text" name="name"/>
+                Titre :<input type="text" name="Titre" value={inputs.title} onChange={handleChange}/>
+            </label>
+            <label>
+                Durée :<input type="text" name="name" value={inputs.time} onChange={handleChange}/>
+            </label>
+            <label>
+                Difficulté :<input type="text" name="name" value={inputs.complexity} onChange={handleChange}/>
+            </label>
+            <label>
+                Pièce :<input type="text" name="name" value={inputs.room} onChange={handleChange}/>
             </label>
         <input type="submit" value="Envoyer" />
         </form>
